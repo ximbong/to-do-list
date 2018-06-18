f09cce947f6a2329f916a156b98a4de50b2fd742
Hai Truong <haitruong181096@gmail.com>
2018-06-18 15:02:25 +0300

    Add Title, ItemList to App.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+import Title from "./components/Title";
+import Input from "./components/Input";
+import Item from "./components/Item";
+
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  addData = newValue => {
+    this.setState({
+      data: [...this.state.data, newValue]
+    });
+  };
+
+  updateData = (data, index) => {
+    const proxy = [...this.state.data];
+    proxy[index] = data;
+
+    this.setState({
+      data: proxy
+    });
+  };
+
   render() {
-    return <div />;
+    const ItemList = this.state.data.map((e, i) => (
+      <Item data={e} index={i} key={i} updateData={this.updateData} />
+    ));
+    console.log(this.state.data);
+    return (
+      <div className="wrapper">
+        <Title />
+        <div className="container">
+          <Input addData={this.addData} />
+          {ItemList}
+        </div>
+      </div>
+    );
   }
 }
 
